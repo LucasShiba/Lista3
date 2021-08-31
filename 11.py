4a90d9d5293cd823b0da8dbb618668a6e4455910
profamaroca <85246612+profamaroca@users.noreply.github.com>
2021-08-31 15:39:34 -0300

    formatando a saída float

--- 11.py
+++ 11.py
@@ -3,6 +3,6 @@ soma = 0
 for i in range(numero_alunos):
     soma += float(input(f'Qual a nota do aluno {i + 1}? '))
 
-print(f'A média é {soma / numero_alunos}')
+print(f'A média é {soma / numero_alunos:.2f}')
 
 
